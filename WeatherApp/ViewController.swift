94d5b435df031e328221aa6156fa0a5e58cec7cb
TaikiFnit <taikifnitspg@gmail.com>
2016-11-13 15:14:57 +0900

    Complete refactoring

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -14,32 +14,30 @@ import SDWebImage
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var todayLabel: UILabel!
-    @IBOutlet weak var todayWeatherImage: UIImageView!
-    @IBOutlet weak var todayWeatherLabel: UILabel!
-    @IBOutlet weak var todayTemperatureLabel: UILabel!
     
-    @IBOutlet weak var tomorrowLabel: UILabel!
-    @IBOutlet weak var tomorrowWeatherImage: UIImageView!
-    @IBOutlet weak var tomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var tomorrowTemperatureLabel: UILabel!
+    @IBOutlet var dateLabelCollection: [UILabel]!
+    @IBOutlet var weatherImageCollection: [UIImageView]!
+    @IBOutlet var weatherLabelCollection: [UILabel]!
+    @IBOutlet var temperatureLabelCollection: [UILabel]!
     
-    @IBOutlet weak var afterTomorrowLabel: UILabel!
-    @IBOutlet weak var afterTomorrowWeatherImage: UIImageView!
-    @IBOutlet weak var afterTomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var afterTomorrowTemperatureLabel: UILabel!
-    
+    let weatherApiUrl: String = "http://weather.livedoor.com/forecast/webservice/json/v1?city=030010"
+
+    // called when view did load
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Alamofire.request("http://weather.livedoor.com/forecast/webservice/json/v1?city=030010").responseJSON {
+        // publish http request
+        Alamofire.request(weatherApiUrl).responseJSON {
             (response: DataResponse<Any>) in
             
+            // error handling: 通信エラーの確認
             if response.result.isFailure == true {
+                // show alert
                 self.simpleAlert(title: "通信エラー", message: "通信に失敗しました")
                 return
             }
             
+            // error handling: 通信結果のチェック
             guard let val = response.result.value as? [String: Any] else {
                 self.simpleAlert(title: "通信エラー", message: "通信結果がJSONではありませんでした")
                 return
@@ -47,50 +45,22 @@ class ViewController: UIViewController {
             
             // responseJSONを使うと辞書形式でも扱えますが、今回はより簡単に扱うためにSwiftyJSONを利用します。
             let json = JSON(val)
-            print(json)
             
-            // タイトル部分
-            if let title = json["title"].string {
-                self.titleLabel.text = title
-            }
+            // タイトル部分(都市名)の表示
+            self.titleLabel.text = json["title"].stringValue
             
             // 天気の情報
             if let forecasts = json["forecasts"].array {
                 
-                // 今日の天気
-                let todayWeather = forecasts[0]
-                
-                self.todayLabel.text = todayWeather["dateLabel"].stringValue
-                
-                if let imgUrl = todayWeather["image"]["url"].string {
-                    self.todayWeatherImage.sd_setImage(with: URL(string: imgUrl))
-                }
-                
-                self.todayWeatherLabel.text = todayWeather["telop"].stringValue
-                self.todayTemperatureLabel.text = self.generateTemperatureText(todayWeather["temperature"])
-                
-                // 明日の天気
-                let tomorrowWeather = forecasts[1]
-                
-                self.tomorrowLabel.text = tomorrowWeather["dateLabel"].stringValue
-                if let imgUrl = tomorrowWeather["image"]["url"].string {
-                    self.tomorrowWeatherImage.sd_setImage(with: URL(string: imgUrl))
-                }
-                self.tomorrowWeatherLabel.text = tomorrowWeather["telop"].stringValue
-                self.tomorrowTemperatureLabel.text = self.generateTemperatureText(tomorrowWeather["temperature"])
-                
-                
-                // 明後日の天気
-                // 気象情報の更新が入るまで明後日の天気情報が存在しない場合があるので要素数をチェックします
-                if forecasts.count >= 3 {
-                    let afterTomorrowWeather = forecasts[2]
-                    
-                    self.afterTomorrowLabel.text = afterTomorrowWeather["dateLabel"].stringValue
-                    if let imgUrl = afterTomorrowWeather["image"]["url"].string {
-                        self.afterTomorrowWeatherImage.sd_setImage(with: URL(string: imgUrl))
-                    }
-                    self.afterTomorrowWeatherLabel.text = afterTomorrowWeather["telop"].stringValue
-                    self.afterTomorrowTemperatureLabel.text = self.generateTemperatureText(afterTomorrowWeather["temperature"])
+                // 要素数分だけ、loop
+                for i in 0 ..< forecasts.count {
+                    // 日付を表示
+                    self.dateLabelCollection[i].text = forecasts[i]["dateLabel"].stringValue
+                    // 天候を表示
+                    self.weatherLabelCollection[i].text = forecasts[i]["telop"].stringValue
+                    // 温度を表示
+                    self.temperatureLabelCollection[i].text = self.generateTemperatureText(forecasts[i]["temperature"])
+                    self.weatherImageCollection[i].sd_setImage(with: URL(string: forecasts[i]["image"]["url"].stringValue))
                 }
             }
         }
@@ -118,10 +88,13 @@ class ViewController: UIViewController {
         return resultText
     }
     
-    // 閉じるボタンのみのアラートを表示します。
+    // 閉じるボタンのみのアラートを表示させるための関数
     func simpleAlert(title: String, message: String) {
+        // alertオブジェクトを生成
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // 閉じるactionを追加
         alert.addAction(UIAlertAction(title: "閉じる", style: .cancel, handler: nil))
+        // alertをshow
         present(alert, animated: true, completion: nil)
     }
 }
